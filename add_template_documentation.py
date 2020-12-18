52cb68f26a5e03a6eb7f9996754ab506d9c8f40a
fmedrano2019 <fmedrano2019@gmail.com>
2020-12-17 23:48:07 -0500

    finalized script

--- add_template_documentation.py
+++ add_template_documentation.py
@@ -1,13 +1,17 @@
 import pywikibot
 import requests
+import re
 import urllib3
 
 # Template to append to pages
 TEMPLATE = "<noinclude>\n\n{{documentation}}\n</noinclude>"
+# Used to prevent a lot of empty space when printing to the console
+TEMPLATE_NO_NEWLINE = TEMPLATE.replace("\n", "")
+
 # The namespace to retrieve pages from
 # https://www.mediawiki.org/wiki/Manual:Namespace#Built-in_namespaces to see available namespaces
 NAMESPACE = 10
-# Number of pages to extract at a time; used in get_pages_json() in params for "aplimit"
+# Number of pages to extract at a time; used in get_params() in params for "aplimit"
 PAGES_LIMIT = 3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -43,26 +47,12 @@ def get_params(continue_from="") -> {}:
 	}
 
 
-def has_template(page_title):
-	"""
-	Checks if the parameter page has TEMPLATE
-
-	:param page_title: title of the page to be scanned
-	:return: True if the page has TEMPLATE; False if otherwise
-	"""
-
-	site = pywikibot.Site()
-	page = pywikibot.Page(site, page_title)
-	page_text = page.text
-	pass
-
-
-def get_pages_to_modify(url) -> []:
+def modify_pages(url: str) -> None:
 	"""
 	Retrieves a Page Generator with all old pages to be tagged
 
 	:param url: String of the path to the API URL of the wiki
-	:return: returns a list of page title
+	:return: None
 	"""
 
 	# Retrieving the JSON and extracting page titles
@@ -72,17 +62,14 @@ def get_pages_to_modify(url) -> []:
 	pages = pages_json["query"]["allpages"]
 	print("Pages to be scanned:", pages)
 
-	pages_to_modify = []
-
-	# Checks the pages for the template
+	# Adds template to the page if needed
 	for page in pages:
 		curr_title = page["title"]
-		if not(has_template(curr_title)):
-			pages_to_modify.append(curr_title)
+		add_template(curr_title)
 
 	if "continue" in pages_json:
 		continue_from = pages_json["continue"]["apcontinue"]
-		print("Continuing from:", continue_from)
+		print("\nContinuing from:", continue_from)
 	else:
 		continue_from = ""
 
@@ -94,56 +81,67 @@ def get_pages_to_modify(url) -> []:
 		pages = pages_json["query"]["allpages"]
 		print("Pages to be scanned:", pages)
 
-		# Checks the pages for the template
+		# Adds template to the page if needed
 		for page in pages:
 			curr_title = page["title"]
-			if not(has_template(curr_title)):
-				pages_to_modify.append(curr_title)
+			add_template(curr_title)
 
 		# Extracting title to continue iterating from
 		if "continue" in pages_json:
 			continue_from = pages_json["continue"]["apcontinue"]
-			print("Continuing from:", continue_from)
+			print("\nContinuing from:", continue_from)
 		else:
 			continue_from = ""
 
-	return pages_to_modify
 
-
-def add_template(pages) -> None:
+def has_template(page_text: str) -> bool:
 	"""
-	Adds the instance variable TEMPLATE to the parameter pages
+	Checks if the parameter page text has a deconstructed version of TEMPLATE in it
 
-	:param pages: pages to be modified; consists of titles
+	:param page_text: page text to be searched
+	:return: True if 'pattern' is in the text; False if otherwise
+	"""
+
+	pattern = '<noinclude>.*{{documentation}}.*</noinclude>'
+	if re.search(pattern, page_text, re.DOTALL | re.IGNORECASE):
+		return True
+	else:
+		return False
+
+
+def add_template(title: str) -> None:
+	"""
+	Checks if the parameter page title has TEMPLATE, adds it if it doesn't
+
+	:param title: string of the page title to be checked and modified
 	:return: None
 	"""
 
 	site = pywikibot.Site()
-	for title in pages:
-		page = pywikibot.Page(site, title)
-		page_text = page.text
+	page = pywikibot.Page(site, title)
+	page_text = page.text
 
-		if page_text.find(TEMPLATE) == -1:
-			print("'%s' not in '%s'... Adding" % (TEMPLATE, page))
-			page_text = u''.join((page_text, TEMPLATE))
-			page.text = page_text
-			page.save(u"Tagged with: " + TEMPLATE, botflag=True)
-		else:
-			print("'%s' already in '%s'... Skipping." % (TEMPLATE, page))
+	if not(has_template(page_text)):
+		print("'%s' not in '%s'... Adding" % (TEMPLATE_NO_NEWLINE, page))
+		page_text = u''.join((page_text, TEMPLATE))
+		page.text = page_text
+		page.save(u"Tagged with: " + TEMPLATE_NO_NEWLINE, botflag=True)
+	else:
+		print("'%s' already in '%s'... Skipping." % (TEMPLATE_NO_NEWLINE, page))
 
 
 def main() -> None:
 	"""
-	Driver. Retrieves all the pages that need to be tagged then tags them.
+	Driver. Iterates through the wiki and adds TEMPLATE where needed.
 	"""
 
+	# Retrieving the wiki URL
 	url = get_api_url()
 	print(url)
 
-	pages_to_modify = get_pages_to_modify(url)
-	add_template(pages_to_modify)
+	modify_pages(url)
 
-	print("No pages left to be tagged")
+	print("\nNo pages left to be tagged")
 
 
 if __name__ == '__main__':
